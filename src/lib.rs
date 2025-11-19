82034a3f320c2da27d0420a5a35ee0c4680a9760
Darien Niamir <darien.niamir@gmail.com>
2025-11-18 22:21:18 -0800

    Move crates under lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 #![no_std]
 
-// Tells the compiler to look for these files
+// Point modules into src/lib/...
+#[path = "lib/chip.rs"]
 pub mod chip;
+
+#[path = "lib/chip_map.rs"]
 pub mod chip_map;
+
+#[path = "lib/bme680.rs"]
 pub mod bme680;
+
+#[path = "lib/led.rs"]
 pub mod led;
